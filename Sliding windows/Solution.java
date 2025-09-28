62d1867726d70d95efc890b18d7acdf3a120d211
OnlySarthak <devsarthak.hotline@gmail.com>
2025-09-28 16:55:53 +0530

    DSA problems on 2025-09-28

--- Sliding windows/Solution.java
+++ Sliding windows/Solution.java
@@ -1,9 +1,90 @@
 import java.util.HashMap;
 
 public class Solution {
+    public int consecetiveOnes3(int[] nums, int k) {
+        // smart sliding windows approach | n
+        int n = nums.length;
+        int left = 0, right = 0;
+        int zeros = 0;
+        int maxLen = 0;
+
+        while (right < n) {
+            if (nums[right] == 0) {
+                zeros++;
+            }
+
+            // if window is invalid (too many zeros),
+            // move left forward by one (not while loop!)
+            if (zeros > k) {
+                if (nums[left] == 0) {
+                    zeros--; // reclaim a bridge
+                }
+                left++; // shrink window by exactly 1
+            }
+
+            // update maxlen ( curr vs old)
+            maxLen = Math.max(maxLen, right - left + 1);
+
+            right++;
+        }
+
+        return maxLen;
+    }
+
+    public int consecetiveOnes2(int[] nums, int k) {
+        // sliding windows approach | 2n
+
+        int n = nums.length;
+        int left = 0, right = 0;
+        int zeros = 0;
+        int maxLen = 0;
+
+        while (right < n) {
+            if (nums[right] == 0) {
+                zeros++;
+            }
+
+            // shrink from the left side if 0 is more than limit
+            while (zeros > k) {
+                if (nums[left] == 0) {
+                    zeros--; // reclaim a bridge
+                }
+                left++;
+            }
+
+            // update maxlen ( curr vs old)
+            maxLen = Math.max(maxLen, right - left + 1);
+
+            right++;
+        }
+
+        return maxLen;
+    }
+
+    public int consecetiveOnes(int[] nums, int k) {
+        // bruteforce | n^2
+        int maxlen = 0, n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int zeros = 0;
+            for (int j = i; j < n; j++) {
+                if (nums[j] == 0)
+                    zeros++;
+
+                if (zeros <= k) {
+                    int tempLen = j - i + 1;
+                    maxlen = Math.max(maxlen, tempLen);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return maxlen;
+    }
+
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
-        int left = 0;  // left pointer
+        int left = 0; // left pointer
         int maxLen = 0; // max length of substring
         HashMap<Character, Integer> map = new HashMap<>(); // stores last index of character
 
