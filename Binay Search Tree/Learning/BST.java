a8059570b623dd7d02f587418dcfd81fb3fd057e
OnlySarthak <devsarthak.hotline@gmail.com>
2025-10-13 23:48:42 +0530

    DSA problems on 2025-10-13

---
+++ Binay Search Tree/Learning/BST.java
@@ -0,0 +1,21 @@
+class BST {
+    class Solution {
+        public TreeNode searchBST(TreeNode root, int val) { //iterative
+            while (root != null && root.val != val) {
+                root = val < root.val ? root.left : root.right;
+            }
+            return root;
+        }
+    }
+
+    public TreeNode searchBST1(TreeNode root, int val) {    //recursive
+        if (root == null || root.val == val) {  
+            return root;
+        }
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
